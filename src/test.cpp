59310dbf72f86b1e3cb774d42382905995415ab8
Zhihao Zhan <zhanzhihao_dt@163.com>
2023-12-05 00:19:12 +0800

    add file header comments

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,12 @@
 /**
- * Copyright (c) 2022 Jose Carlos Garcia (jcarlos3094@gmail.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author Jose Carlos Garcia
- *
+ * \file test.cpp
+ * \author Zhihao Zhan (zhanzhihao_dt@163.com)
+ * \brief test nano dbscan
+ * \version 0.1
+ * \date 2023-12-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
  */
 
 #include <iostream>
